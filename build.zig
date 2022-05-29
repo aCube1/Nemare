56f6aaee42f9342d9f663d4c9b2e282b190066fb
aCube <player559001@gmail.com>
2022-05-29 19:45:10 -0300

    Adds stb_image.

--- build.zig
+++ build.zig
@@ -9,17 +9,7 @@ pub fn build(b: *Builder) void {
     nemare.setTarget(target);
     nemare.setBuildMode(mode);
     nemare.linkLibC();
-    switch (target.getOsTag()) {
-        .linux => nemare.linkSystemLibrary("SDL2"),
-        .windows => {
-                        // Change this according to where your SDL2 source files are located.
-                        const SDL2_PATH = "C:/vclib/SDL2-2.0.22/";
-                        nemare.addIncludeDir(SDL2_PATH ++ "include");
-                        nemare.addLibPath(SDL2_PATH ++ "lib/x64");
-                        nemare.linkSystemLibrary("SDL2");
-        },
-        else => std.debug.panic("Cannot identify current OS", .{}),
-    }
+    linkCLibraries(nemare, target.getOsTag());
     nemare.install();
 
     const nemare_cmd = nemare.run();
@@ -30,4 +20,30 @@ pub fn build(b: *Builder) void {
 
     const nemare_step = b.step("run-nemare", "Run Nemare");
     nemare_step.dependOn(&nemare_cmd.step);
+}
+
+pub fn linkCLibraries(
+    exe: *std.build.LibExeObjStep,
+    os_tag: std.Target.Os.Tag,
+) void {
+    const c_flags = [_][]const u8{
+        "-std=c99",
+        "-O2",
+    };
+
+    // Link stb_image
+    exe.addCSourceFile("src/deps/stb/image.c", &c_flags);
+
+    // Link SDL
+    switch (os_tag) {
+        .linux => exe.linkSystemLibrary("SDL2"),
+        .windows => {
+            // Change this according to where your SDL2 source files are located.
+            const SDL2_PATH = "C:/vclib/SDL2-2.0.22/";
+            exe.addIncludeDir(SDL2_PATH ++ "include");
+            exe.addLibPath(SDL2_PATH ++ "lib/x64");
+            exe.linkSystemLibrary("SDL2");
+        },
+        else => std.debug.panic("Unable to identify current OS", .{}),
+    }
 }
---
+++ src/deps/stb/image.c
@@ -0,0 +1,5 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
+/* NOTE: Uncomment next line to disable TGA loading, not sure if TGA can cause trouble */
+/* #define STBI_NO_TGA */
+#include "image.h"
